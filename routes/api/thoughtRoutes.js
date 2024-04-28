569f28ad5e5cc82e9b418ecd36df8edcba80daac
Matthew Lowery <matthewdlowery@msn.com>
2024-04-28 16:58:48 -0500

    Create thought and user routes, and configured api index file

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThought).post(createThought);
+
+router.route('/:thoughtID')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtID/reactions')
+.post(createReaction);
+
+router.route('/:thoughtID/reactions/:reactionID')
+.delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require ('../../controllers/userController');
+
+router.route('/').get(getUser).post(createUser);
+
+router.route('/:userID')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route('/:userID/friends/:friendID')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
